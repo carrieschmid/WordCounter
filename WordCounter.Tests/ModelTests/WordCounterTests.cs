1760ffd0d23edaa8ac8997dbeb5a9a00f3c5094a
Carrie Schmid <carriepederson7@gmail.com>
2019-10-05 10:27:35 -0700

    Add funtion to check inputs for letters only.

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -5,51 +5,41 @@ namespace WordCounter.Tests {
 
     [TestClass]
     public class WordCounterTest {
-        [TestMethod]
-        public void RemovePunc_RemovePuncFromPhrase_String()
-        {
-            //Arrange
-            CountWords newCountWords = new CountWords();
-            //Act
-            string result = newCountWords.RemovePunc("I love my cat.");
-            //Assert
-            Assert.AreEqual(result, "I love my cat");
-        }
-
         // [TestMethod]
+        // public void RemovePunc_RemovePuncFromPhrase_String()
+        // {
+        //     //Arrange
+        //     CountWords newCount = new CountWords();
+        //     //Act
+        //     string result = newCount.RemovePunc("I love my cat.");
+        //     //Assert
+        //     Assert.AreEqual(result, "I love my cat");
+        // }
+
+        //   [TestMethod]
         // public void FindWord_FindInputWordInPhrase_Int()
         // {
         //     //Arrange
         //     CountWords newCountWords = new CountWords();
         //     //Act
-        //     int result = newCountWords.FindWord("cat" , "I love my cat and cat.");
+        //     int result = newCountWords.FindWord("cat" , "I love my cat and cat");
         //     //Assert
         //     Assert.AreEqual(result, 2);
         // }
 
-        // CountWords counter;
+        [TestMethod]
+        public void LettersOnly_MakeLettersOnlyPhrasesTrue_True()
+        {
+            //Arrange
+            CountWords newCount = new CountWords();
+            //Act
+            bool result = newCount.LettersOnly("I love my cat");
+            //Assert
+            Assert.AreEqual(result, false);
+        }
+      
 
-        // [TestInitialize]
-        // public void Startup () {
-        //     counter = new CountWords ();
-
-        // }
-
-        // [TestCleanup]
-        // public void TestClean () {
-        //     counter = null;
-        // }
-
-        // [TestMethod]
-        // public void CheckUpperInput_CheckForNonAlphabeticUpperInputs_True () {
-        //     Assert.AreEqual (true, counter.CheckUpperInput ("E"));
-        // }
-
-        //   [TestMethod]
-        // public void FindWord_FindInputWordInText_Int() {
-        //     Assert.AreEqual (1, counter.FindWord ("my cat went to the park."));
-        // }
-
+        
 
     }
 }
--- WordCounter/Models/WordCouter.cs
+++ WordCounter/Models/WordCouter.cs
@@ -9,95 +9,78 @@ namespace WordCounter.Models
         char[] AlphabeticUpper { get; set;}
         int WordCount { get; set; }
         // string InputWord { get; set; }
-        // string InputText { get; set;}
+        // string InputText { get; set; }
        public CountWords () 
        {
-           char [] alphabet = new char[]{' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
            int WordCount = 0;
-        //    string InputWord = inputWord;
-        //    string InputText = inputText;
+        //    string InputWord = "";
+        //    string InputText = "";
             
         }
 
 
-        public string RemovePunc (string input)
-        {
-            var charsToRemove = new string[] { "@", ",", ".", ";", "'" };
-            foreach (var c in charsToRemove)
+        // public string RemovePunc (string input)
+        // {
+        //     var charsToRemove = new string[] { "@", ",", ".", ";", "'" };
+        //     foreach (var c in charsToRemove)
+        //     {
+        //         input = input.Replace(c, string.Empty);
+        //     }
+        //     return input;
+
+        // }
+               
+        // public int FindWord(string inputWord, string inputText)
+        // {   
+        // CountWords newCountWords = new CountWords();
+        // char[] spearator = {' '}; 
+        // String[] inputArray = inputText.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
+        // int result = 0;
+        // foreach(string word in inputArray)
+        // { 
+        //     if(inputWord == word)
+        //     {
+        //         newCountWords.WordCount ++;
+        //         result = newCountWords.WordCount;
+        //     }
+        //     else
+        //     {
+        //         result = 0;
+        //     }
+        //     }
+        //     return result;
+        // }
+  
+
+    public bool LettersOnly(string input)
+       {
+            bool result = true;
+            char[] inputArray = input.ToCharArray();
+            foreach(char letter in inputArray)
             {
-                input = input.Replace(c, string.Empty);
-            }
-            string finalInput = "";
-            foreach(char letter in input)
-            {
-                
-                bool isLetter = char.IsLetter(letter);
+                bool isLetter = Char.IsLetter(letter);
                 {
                     if(isLetter)
                     {
-                        finalInput = input;                   
+                        result=  true;         
                     }
                     else
                     {
-                        finalInput = "Please remove characters.";
-                    }
-                    
-
-                    return finalInput;
+                        result = false;
+                    }       
                 }
+               
             }
-        }
+            return result; 
+       }
 
 
-    
-            // inputString = new string((from c in inputString
-            //       where char.IsWhiteSpace(c) || char.IsLetterOrDigit(c)
-            //       select c).ToArray());
-
-        //     char[] array = input.ToCharArray();
-        //     char[] AlphabeticUpper = new char[25];
-        //     for (int index = 0; index<= array.Length; index ++) 
-        //         for(int j = 0; j <=AlphabeticUpper.Length; j ++)
-                
-        //     {
-        //         if(array[index] == AlphabeticUpper[j]  )
-        //         {
-        //             return true;
-        //         }
-        //     }
-        // return false;
-
-        
-
-
-
-        public int FindWord(string inputWord, string inputText)
-        {   
-        CountWords newCountWords = new CountWords();
-
-        char[] spearator = {' '}; 
-
-        String[] inputArray = inputText.Split(spearator,  
-           StringSplitOptions.RemoveEmptyEntries);
-        int result = 0;
-        foreach(string word in inputArray)
-        { 
-            if(inputWord == word)
-            {
-                newCountWords.WordCount ++;
-                result = newCountWords.WordCount;
-            }
-            else
-            {
-                result = 0;
-            }
-            }
-            return result;
-        }
-  
-
-        }    
+    }    
       
     
-    }
+}
 
+
+
+
+   
